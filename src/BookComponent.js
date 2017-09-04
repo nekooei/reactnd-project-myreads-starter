7eeedf0fefc954a8d97d3f8ec186cfc8229aab91
milad <mi.nekooei@gmail.com>
2017-09-04 12:09:53 +0430

    Attach Update Function for each book

--- src/BookComponent.js
+++ src/BookComponent.js
@@ -9,28 +9,29 @@ import escapeRegExp from 'escape-string-regexp'
 
 
 class BookComponent extends Component{
+
+
     static propTypes = {
-        Book: PropTypes.object.isRequired
+        Book: PropTypes.object.isRequired,
+        updateBookStatus : PropTypes.func.isRequired
     }
 
     render (){
-        const { Book } = this.props
+        const { Book , updateBookStatus} = this.props
         return(
             <div className="book">
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${Book.imageLinks.thumbnail})` }}></div>
                     <div className="book-shelf-changer">
-                        <select>
-                            <option value="none" disabled>Move to...</option>
+                        <select onChange={(event) => updateBookStatus(Book, event.target.value)}>
+                            <option value="none">Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
                             <option value="read">Read</option>
-                            <option value="none">None</option>
                         </select>
                     </div>
                 </div>
                 <div className="book-title">{Book.title}</div>
-                {console.log(Book.authors)}
                 {Book.authors !== undefined ? (
                     Book.authors.map(author => (
                         <div className="book-authors">{author}</div>
