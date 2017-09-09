fbb333b93507bd813b6144672c50c5a08651a980
milad nekooei <mi.nekooei@gmail.com>
2017-09-09 18:17:53 +0430

    convert let to const

--- src/App.js
+++ src/App.js
@@ -15,9 +15,9 @@ class BooksApp extends React.Component {
 
     componentDidMount() {
         BooksAPI.getAll().then(Books => {
-            let wantToRead = Books.filter(book => book.shelf === 'wantToRead')
-            let currentlyReading = Books.filter(book => book.shelf === 'currentlyReading')
-            let read = Books.filter(book => book.shelf === 'read');
+            const wantToRead = Books.filter(book => book.shelf === 'wantToRead')
+            const currentlyReading = Books.filter(book => book.shelf === 'currentlyReading')
+            const read = Books.filter(book => book.shelf === 'read');
             this.setState({
                 CurrentlyReading : currentlyReading
                 , WantToRead : wantToRead
